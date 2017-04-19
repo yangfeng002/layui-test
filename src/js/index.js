a74b43cf15364a41517ce1047ea5a866f9994a7c
fengyang <yangfeng123450908@126.com>
2017-04-19 09:42:40 +0800

    layui框架文件引入

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by fengyang on 2017/4/19.
+ */
+//自定义函数
+layui.define(function(exports){
+    exports('demo', function(){
+        alert('Hello World!');
+    });
+});
+layui.demo();
+
